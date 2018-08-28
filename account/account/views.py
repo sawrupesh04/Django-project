fe9d534753f5e8337122a48edd166f929121e158
Rupesh Kumar Saw <rupeshsaw786.com>
2018-08-28 22:16:23 +0530

    django-project

---
+++ account/account/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "index.html", context)
+
+def signup(request):
+    if request.method == 'POST':
+        # user has info and want to sign up
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'signup.html', {'error': 'User name already exits.Try again with different username'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('index')
+        else:
+            return render(request, 'signup.html', {'error': 'password must match!'})
+
+
+    return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error': 'Username or password incorrect!'})
+
+    else:
+        return render(request, 'login.html')
